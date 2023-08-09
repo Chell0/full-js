ee3567a63fd67c4ce93fb7779375a217662c6836
chelloh <machelgabriel@gmail.com>
2023-08-09 14:13:19 +0300

    - Install eslint and prettier package - Configure prettier with eslint intergration - Update .gitignore file

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 2,
+    singleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    semi: false,
+
+    // ESLint settings
+    eslintIntegration: true,
+    // Add more ESLint-related settings if needed
+};
